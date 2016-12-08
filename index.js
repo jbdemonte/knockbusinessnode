d076d6ac6ec1aa5bb52a98e044f0068487c9f1d4
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2016-12-08 15:57:12 +0100

    Version 1.0

---
+++ index.js
@@ -0,0 +1,109 @@
+var unix = require('unix-dgram');
+var microtime = require('microtime');
+
+module.exports = Knock;
+
+var BUFFER_LEN = 704;
+
+function Knock(socketPath) {
+
+  socketPath = socketPath || '/var/run/knockdaemon.udp.socket';
+  var counters = {};
+  var gauges = {};
+  var delays = [];
+
+  this.gauge = cleanDecorator(this, true, function (item, value) {
+    gauges[item] = value;
+  });
+
+  this.increment = cleanDecorator(this, true, function (item, value) {
+    counters[item] = (counters[item] || 0) + value;
+  });
+
+  this.start_delay = cleanDecorator(this, false, function (item) {
+    return [item, microtime.now()];
+  });
+
+  this.stop_delay = function (timecode) {
+    if (!Array.isArray(timecode) || timecode.length !== 2 || !isClean(timecode[0]) || !isNumeric(timecode[1])) {
+      return false;
+    }
+    delays.push([timecode[0], 'DTC', (microtime.now() - timecode[1]) / 1000]);
+    return true;
+  };
+
+  this.commit = function () {
+    var items = delays.concat(
+      Object.keys(gauges).map(function (name) {
+        return [name, 'G', gauges[name]];
+      }),
+      Object.keys(counters).map(function (name) {
+        return [name, 'C', counters[name]];
+      })
+    );
+
+    // reset
+    counters = {};
+    gauges = {};
+    delays = [];
+
+    return send(socketPath, items);
+  };
+
+}
+
+/**
+ * Decorate a function with isClean
+ * @param {object} self
+ * @param {boolean} testValue
+ * @param {function} fn
+ * @return {Function}
+ */
+function cleanDecorator(self, testValue, fn) {
+  return function (item, value) {
+    if (isClean(item) && (!testValue || isNumeric(value))) {
+      var result = fn.apply(self, arguments);
+      return result === undefined || result;
+    }
+    return false;
+  };
+}
+
+/**
+ * Return True if the item is well formatted
+ * @param {string} item
+ * @return {boolean}
+ */
+function isClean(item) {
+  return Boolean(item && typeof item === 'string' && item.length <= 70);
+}
+
+/**
+ * Send data to the socket
+ * @param {string} path
+ * @param {Array} items
+ * @return {number}
+ */
+function send(path, items) {
+  var len = items.length;
+  if (len) {
+    var message;
+    var client = unix.createSocket('unix_dgram');
+    client.on('error', console.error);
+    while (items.length) {
+      message = Buffer(JSON.stringify(items.splice(0, BUFFER_LEN)));
+      client.send(message, 0, message.length, path);
+    }
+    client.close();
+  }
+  return len;
+}
+
+/**
+ * Return True if the value is a numeric
+ * @param {*} value
+ * @return {boolean}
+ */
+function isNumeric(value) {
+  return typeof value === "number" && Number.isFinite(value);
+}
---
+++ test/index.js
@@ -0,0 +1,276 @@
+var unix = require('unix-dgram');
+var microtime = require('microtime');
+var fs = require('fs');
+var expect = require('chai').expect;
+var Knock = require('../index');
+
+var socketFile = __dirname + '/unit.tests.knockprobe';
+
+describe('knockprobe', function () {
+  var server, received = [], chunks = [];
+
+  before(function () {
+    server = unix.createSocket('unix_dgram', function (buf) {
+      received = received.concat(JSON.parse(buf));
+      chunks.push(JSON.parse(buf));
+    });
+    server.bind(socketFile);
+  });
+
+  beforeEach(function () {
+    received = [];
+    chunks = [];
+  });
+
+  after(function () {
+    server.close();
+    fs.unlink(socketFile);
+  });
+
+  function wait(fn, delay) {
+    setTimeout(fn, delay || 25);
+  }
+
+  it('should not autocommit a gauge', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.gauge('apple', 2)).to.eql(true);
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  [true, false, null, undefined, {}, [], '', (new Array(100)).join('A')].forEach(function (name) {
+    it('should reject a bad name in a gauge: ' + JSON.stringify(name), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.gauge(name, 2)).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+  });
+
+  [true, false, null, undefined, {}, []].forEach(function (value) {
+    it('should reject a bad value in a gauge: ' + JSON.stringify(value), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.gauge('apple', value)).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+  });
+
+  it('should send a simple gauge', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.gauge('apple', 2)).to.eql(true);
+    expect(k.commit()).to.eql(1);
+    wait(function () {
+      expect(received).to.eql([[ 'apple', 'G', 2 ]]);
+      done();
+    });
+  });
+
+  it('should send multiple gauges', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.gauge('apple', 2)).to.eql(true);
+    expect(k.gauge('orange', 3)).to.eql(true);
+    expect(k.gauge('apple', 4)).to.eql(true);   // should overwrite previous "apple"
+
+    expect(k.commit()).to.eql(2);
+
+    wait(function () {
+      expect(received).to.eql([[ 'apple', 'G', 4 ], [ 'orange', 'G', 3 ]]);
+      done();
+    });
+  });
+
+  it('should not autocommit an increment', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.increment('action', 2)).to.eql(true);
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  [true, false, null, undefined, {}, [], '', (new Array(100)).join('A')].forEach(function (name) {
+    it('should reject a bad name in a increment: ' + JSON.stringify(name), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.increment(name, 2)).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+  });
+
+  [true, false, null, undefined, {}, []].forEach(function (value) {
+    it('should reject a bad value in a increment: ' + JSON.stringify(value), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.increment('action', value)).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+  });
+
+  it('should send a an increment', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.increment('action', 2)).to.eql(true);
+    expect(k.commit()).to.eql(1);
+    wait(function () {
+      expect(received).to.eql([[ 'action', 'C', 2 ]]);
+      done();
+    });
+  });
+
+  it('should send multiple increments', function (done) {
+    var k = new Knock(socketFile);
+
+    expect(k.increment('action1', 12)).to.eql(true);
+    expect(k.increment('action2', 1)).to.eql(true);
+    expect(k.increment('action1', -3)).to.eql(true); // should be merge with previous action 1
+
+    expect(k.commit()).to.eql(2);
+
+    wait(function () {
+      expect(received).to.eql([[ 'action1', 'C', 9 ], [ 'action2', 'C', 1 ]]);
+      done();
+    });
+  });
+
+  [true, false, null, undefined, {}, [], '', (new Array(100)).join('A')].forEach(function (name) {
+    it('should reject a bad name in a start_delay: ' + JSON.stringify(name), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.start_delay(name)).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+  });
+
+  it('should should not send anything on start_delay', function (done) {
+    var k = new Knock(socketFile);
+    var timecode = k.start_delay('api_facebook_request');
+
+    expect(timecode).to.be.an('array');
+    expect(timecode.length).to.eql(2);
+    expect(timecode[0]).to.eql('api_facebook_request');
+    expect(timecode[1]).to.be.a('number');
+
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  it('should not send anything on start_delay', function (done) {
+    var k = new Knock(socketFile);
+    var timecode = k.start_delay('api_facebook_request');
+
+    expect(timecode).to.be.an('array');
+    expect(timecode.length).to.eql(2);
+    expect(timecode[0]).to.eql('api_facebook_request');
+    expect(timecode[1]).to.be.a('number');
+
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  it('should not autocommit a stop_delay', function (done) {
+    var k = new Knock(socketFile);
+    var timecode = k.start_delay('api_facebook_request');
+    expect(k.stop_delay(timecode)).to.eql(true);
+
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  it('should reject a missing timecode in a stop_delay', function (done) {
+    var k = new Knock(socketFile);
+    expect(k.stop_delay()).to.eql(false);
+    expect(k.commit()).to.eql(0);
+    wait(function () {
+      expect(received).to.eql([]);
+      done();
+    });
+  });
+
+  [
+    true, false, null, undefined, 0, 1, {}, [],
+    ['api_facebook_request'],
+    ['api_facebook_request', microtime.now(), 'something else'],
+    ['', microtime.now()],
+    [(new Array(100)).join('A'), microtime.now()]
+  ].forEach(function (timecode) {
+
+    it('should reject an invalid timecode in a stop_delay: ' + JSON.stringify(timecode), function (done) {
+      var k = new Knock(socketFile);
+      expect(k.stop_delay()).to.eql(false);
+      expect(k.commit()).to.eql(0);
+      wait(function () {
+        expect(received).to.eql([]);
+        done();
+      });
+    });
+
+  });
+
+  it('should send a delay', function (done) {
+    var k = new Knock(socketFile);
+    var timecode = k.start_delay('api_facebook_request');
+
+    setTimeout(function () {
+      expect(k.stop_delay(timecode)).to.eql(true);
+      expect(k.commit()).to.eql(1);
+
+      wait(function () {
+        expect(received.length).to.eql(1);
+        received = received.shift();
+        expect(received[0]).to.eql('api_facebook_request');
+        expect(received[1]).to.eql('DTC');
+        expect(received[2]).to.within(290, 350);
+        done();
+      });
+
+    }, 300);
+  });
+
+  it('should handle pagination when sending tons of events', function (done) {
+    var k = new Knock(socketFile);
+    var len = 30000;
+    var BUFFER_LEN = 704; // voluntary set here, instead of getting it from Knock.BUFFER_LEN for example
+
+    for (var i=0; i < len; i++) {
+      expect(k.gauge('gauge' + i, i)).to.eql(true);
+    }
+
+    expect(k.commit()).to.eql(len);
+
+    wait(function () {
+      expect(received.length).to.eql(len);
+      for (var i=0; i < len; i++) {
+        expect(received[i]).to.eql([ 'gauge' + i, 'G', i]);
+      }
+      expect(chunks.length).to.eql(Math.ceil(len / BUFFER_LEN));
+      chunks.forEach(function (chunk, index) {
+        expect(chunk.length).to.eql(index < chunks.length - 1 ? BUFFER_LEN : len % BUFFER_LEN);
+      });
+      done();
+    }, 300);
+  });
+
+});
